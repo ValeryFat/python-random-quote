cd15940b1056601687e857b91fbcbb86920475e4
ValeryFat <Valery.fateyev@gmail.com>
2021-06-15 19:37:46 +0300

    Hello World

--- get-quote.py
+++ get-quote.py
@@ -5,9 +5,15 @@ def one():
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
-  last = 13
+
+  last = 16
   rnd = random.randint(0,last)
-  print(quotes[rnd])
+  print(quotes[rnd], end=' ', sep='\n')
+  print(quotes[rnd+1])
+
+  file = open("quotes.txt", "a")
+  file.write("\nHello Word")
+  file.close()
 
 if __name__== "__main__":
   one()
